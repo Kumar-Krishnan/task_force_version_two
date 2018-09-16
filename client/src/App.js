30b1ac2052ffb6a5bee9ed17bbaea5ab61aef2ce
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2018-09-16 19:01:26 -0400

    test post components and functions built and functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
 import SignUpLogin from './components/SignUpLogIn'
+import PostsList from './components/PostsList'
 import axios from 'axios'
 
 class App extends Component {
   state = {
-    signedIn: false
+    signedIn: false,
+    posts: []
   }
+
+  async componentWillMount() {
+    try {
+        let posts = []
+        if (this.state.signedIn) {
+            posts = await this.getPosts()
+        }
+
+        this.setState({
+            posts
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+
+  getPosts = async() =>{
+      try{
+          const response = await axios.get('/posts')
+          return response.data
+      } catch (error){
+          console.log(error)
+          return []
+      }
+  }
+
 
   signUp = async (email,password, password_confirmation) => {
     try {
@@ -48,14 +77,21 @@ class App extends Component {
         signIn={this.signIn}
       />
     )
+
+    const PostsComponent = () => (
+      <PostsList
+          posts={this.state.posts}/>
+    )
+    
     return (
       <Router>
           <div>
               <Switch>
                 <Route exact path="/signUp" render = {SignUpLogInComponent}/>
+                <Route exact path="/posts" render={PostsComponent}/>
               </Switch>
 
-              {this.state.signedIn ? null: <Redirect  to="/signUp"/>}
+              {this.state.signedIn ?  <Redirect to="/posts"/> : <Redirect  to="/signUp"/>}
           </div>
       </Router>
     );
